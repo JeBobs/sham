f5aae1e049ddfb37f0bbf890897c49cd2cf2b401
Nathan <jebcraftserver@gmail.com>
2021-11-07 22:20:40 -0500

    updated help

--- Sham!/CommandDirectory.cs
+++ Sham!/CommandDirectory.cs
@@ -20,7 +20,7 @@ namespace Sham
             "help <command> - Either shows this screen or provides help for a command.",
             "GenerateH2Shaders [JMS File] - Generates empty Halo 2 compatible shader files for each material slot.",
             "GenerateH3Shaders [JMS File] - Generates empty Halo 3 compatible shader files for each material slot.",
-            "JMCompress [JMS/JMA File] - Takes an input JMS/JMA/JMM file and removes white-space & comments."
+            "JMCompress [JMS/JMA Variant File] <output path> - Takes an input JMS/JMA/JMM/JMO/JMI file and removes white-space & comments."
         };
     }
 }
