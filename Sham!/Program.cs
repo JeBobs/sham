a05af7203865e0c1b81025bde29a206242f4ed55
JeBobs <jebcraftserver@gmail.com>
2022-12-26 11:44:32 -0500

    small include cleanup

--- Sham!/Program.cs
+++ Sham!/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.IO;
 using System.Reflection;
-using System.Collections.Generic;
 
 using static Sham.CommandDirectory;
 using static Sham.UserInterface;
